dfe7e7b4b3ec21b8384772240f7c071b142ada35
wangn9900 <57337569+wangn9900@users.noreply.github.com>
2022-04-28 02:32:13 +0800

    Update inboundbuilder_test.go

--- service/controller/inboundbuilder_test.go
+++ service/controller/inboundbuilder_test.go
@@ -3,8 +3,8 @@ package controller_test
 import (
 	"testing"
 
-	"github.com/XrayR-project/XrayR/api"
-	. "github.com/XrayR-project/XrayR/service/controller"
+	"github.com/wangn9900/XrayR/api"
+	. "github.com/wangn9900/XrayR/service/controller"
 )
 
 func TestBuildV2ray(t *testing.T) {
