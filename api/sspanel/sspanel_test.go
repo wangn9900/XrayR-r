0fd4cff36d5baa8c0f8a95cf8d1943d2c1a2b3d9
wangn9900 <57337569+wangn9900@users.noreply.github.com>
2022-04-28 02:23:01 +0800

    Update sspanel_test.go

--- api/sspanel/sspanel_test.go
+++ api/sspanel/sspanel_test.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/XrayR-project/XrayR/api"
-	"github.com/XrayR-project/XrayR/api/sspanel"
+	"github.com/wangn9900/XrayR/api"
+	"github.com/wangn9900/XrayR/api/sspanel"
 )
 
 func CreateClient() api.API {
