fb24002baed5eccfd0168374c93e79102854710c
wangn9900 <57337569+wangn9900@users.noreply.github.com>
2022-04-28 02:24:01 +0800

    Update proypanel_test.go

--- api/proxypanel/proypanel_test.go
+++ api/proxypanel/proypanel_test.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/XrayR-project/XrayR/api"
-	"github.com/XrayR-project/XrayR/api/proxypanel"
+	"github.com/wangn9900/XrayR/api"
+	"github.com/wangn9900/XrayR/api/proxypanel"
 )
 
 func CreateClient() api.API {
