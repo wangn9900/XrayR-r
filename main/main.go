f8dae01f6616732059cece3e6aaa8253c5bbdffe
wangn9900 <57337569+wangn9900@users.noreply.github.com>
2022-04-28 02:17:47 +0800

    Update main.go

--- main/main.go
+++ main/main.go
@@ -12,7 +12,7 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/XrayR-project/XrayR/panel"
+	"github.com/wangn9900/XrayR/panel"
 	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/viper"
 )
@@ -23,7 +23,7 @@ var (
 )
 
 var (
-	version  = "0.8.0"
+	version  = "0.8.1"
 	codename = "XrayR"
 	intro    = "A Xray backend that supports many panels"
 )
