3d7b8bd37de883380a553ca9d688fac79ef511ad
wangn9900 <57337569+wangn9900@users.noreply.github.com>
2022-04-28 02:31:17 +0800

    Update controller_test.go

--- service/controller/controller_test.go
+++ service/controller/controller_test.go
@@ -8,10 +8,10 @@ import (
 	"syscall"
 	"testing"
 
-	"github.com/XrayR-project/XrayR/api"
-	"github.com/XrayR-project/XrayR/api/sspanel"
-	_ "github.com/XrayR-project/XrayR/main/distro/all"
-	. "github.com/XrayR-project/XrayR/service/controller"
+	"github.com/wangn9900/XrayR/api"
+	"github.com/wangn9900/XrayR/api/sspanel"
+	_ "github.com/wangn9900/XrayR/main/distro/all"
+	. "github.com/wangn9900/XrayR/service/controller"
 	"github.com/xtls/xray-core/core"
 	"github.com/xtls/xray-core/infra/conf"
 )
